eba0139a26bd68422f10e545bbff350de32a0284
himatentu <himatentu17@gmail.com>
2025-09-06 12:34:13 +0530

    data types

--- practice.js
+++ practice.js
@@ -1,1 +1,34 @@
 console.log("hello ,Hima")
+
+let name="Hima";
+console.log(name)
+
+let n=61;
+console.log(n)
+
+let i=false;
+console.log(i)
+
+let x;
+console.log(x)
+
+let y=null;
+console.log(y);
+
+console.log(typeof name);
+console.log(typeof n);
+console.log(typeof i);
+console.log(typeof x);
+console.log(typeof y);
+
+let arr=[1,2,3,4,5];
+console.log(arr[1])
+console.log(typeof arr)
+
+let person={
+    name:"hima",
+    age:18
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
